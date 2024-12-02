d3f01a9ccb26338a9abea6e95a253db07adc8553
Jira Pit <pitakwongjira@gmail.com>
2024-12-02 23:50:42 +0700

    add install.sh

---
+++ install.sh
@@ -0,0 +1,25 @@
+# install shared packages
+sudo apt update
+sudo apt install -y \
+  pkg-config \
+  build-essential \
+  libopencv-dev \
+  libclang-dev \
+  libbluetooth-dev \
+  libdbus-1-dev \
+  bluez \
+  v4l-utils \
+  libssl-dev \
+  clang \
+  wget
+
+# download the latest release and extreact it
+wget -O release.tar.gz https://github.com/CMKL-PathSense/PathSense-System-Releases/releases/download/beta/release.tar.gz
+tar -xvf release.tar.gz
+rm release.tar.gz
+mv release pathsense_system_installer
+
+# install the release
+cd pathsense_system_installer
+chomod +x pathsense-system
+./pathsense-system install
